a6e03e62acfcff5c7f6d2a8826fbd993c89ba5c2
BoozieBoi <michael.heinecke.1337@gmail.com>
2020-05-15 00:41:19 +0200

    Code for section 8 * Reading from disk

---
+++ src/main/java/com/apachsparkforjavadevelopers/rddoperations/ReadingFromDisk.java
@@ -0,0 +1,28 @@
+package com.apachsparkforjavadevelopers.rddoperations;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Arrays;
+
+public class ReadingFromDisk {
+
+    public static void main(String[] args) {
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> fromDiskRdd = sc.textFile("src/main/resources/subtitles/input.txt");
+
+        fromDiskRdd.flatMap(line -> Arrays.asList(line.split(" ")).iterator())
+                .collect()
+                .forEach(System.out::println);
+
+        sc.close();
+    }
+}
