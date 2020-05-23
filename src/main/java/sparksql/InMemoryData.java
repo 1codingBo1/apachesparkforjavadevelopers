17f0149c56ff7de8bd77d664ec7dd9081d1939fe
BoozieBoi <michael.heinecke.1337@gmail.com>
2020-05-23 17:08:35 +0200

    Code for In Memory Data (section 19) * Creating a dataset from a list and a schema

---
+++ src/main/java/sparksql/InMemoryData.java
@@ -0,0 +1,46 @@
+package sparksql;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryData {
+    public static void main(String[] args) {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder().appName("datasetBasics").master("local[*]")
+                .config("spark.sql.warehouse.dir", "file:///C:/Users/mheinecke/spark_tmp")
+                .getOrCreate();
+
+        // Create list of Row objects
+        List<Row> inMemory = new ArrayList<>();
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 04:19:32"));
+        inMemory.add(RowFactory.create("FATAL", "2016-12-31 03:22:34"));
+        inMemory.add(RowFactory.create("WARN", "2016-12-31 03:21:21"));
+        inMemory.add(RowFactory.create("INFO", "2015-4-21 14:32:21"));
+        inMemory.add(RowFactory.create("FATAL", "2015-4-21 19:23:20"));
+
+        // Create the dataset schema
+        StructField[] fields = new StructField[]{
+                new StructField("level", DataTypes.StringType, false, Metadata.empty()),
+                new StructField("datetime", DataTypes.StringType, false, Metadata.empty())
+        };
+
+        StructType schema = new StructType(fields);
+        Dataset<Row> dataset = spark.createDataFrame(inMemory, schema);
+
+        dataset.show();
+
+        spark.close();
+    }
+}
