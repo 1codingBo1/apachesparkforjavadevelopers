b3c2616dc650ec9baeaf9fa902c14370a18e67cb
BoozieBoi <michael.heinecke.1337@gmail.com>
2020-06-08 00:04:45 +0200

    Added categorical column gender to model

--- src/main/java/com/apachesparkforjavadevelopers/ml/GymCompetitors.java
+++ src/main/java/com/apachesparkforjavadevelopers/ml/GymCompetitors.java
@@ -2,6 +2,8 @@ package com.apachesparkforjavadevelopers.ml;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.spark.ml.feature.OneHotEncoderEstimator;
+import org.apache.spark.ml.feature.StringIndexer;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.regression.LinearRegression;
 import org.apache.spark.ml.regression.LinearRegressionModel;
@@ -24,8 +26,18 @@ public class GymCompetitors {
                 .option("inferSchema", true)
                 .csv("src/main/resources/gymcompetition/GymCompetition.csv");
 
+        StringIndexer genderIndexer = new StringIndexer()
+                .setInputCol("Gender")
+                .setOutputCol("GenderIndex");
+        csvData = genderIndexer.fit(csvData).transform(csvData);
+
+        OneHotEncoderEstimator genderEncoder = new OneHotEncoderEstimator()
+                .setInputCols(new String[]{"GenderIndex"})
+                .setOutputCols(new String[]{"GenderVector"});
+        csvData = genderEncoder.fit(csvData).transform(csvData);
+
         VectorAssembler vectorAssembler = new VectorAssembler();
-        vectorAssembler.setInputCols(new String[]{"Age", "Height", "Weight"});
+        vectorAssembler.setInputCols(new String[]{"Age", "Height", "Weight", "GenderVector"});
         vectorAssembler.setOutputCol("features");
         Dataset<Row> csvDataWithFeatures = vectorAssembler.transform(csvData);
 
