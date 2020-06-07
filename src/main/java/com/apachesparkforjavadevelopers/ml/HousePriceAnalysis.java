0132e15ea5b84bb2b7dee383ad7bbdb64d72eed3
BoozieBoi <michael.heinecke.1337@gmail.com>
2020-06-07 23:23:25 +0200

    Linear regression model based on house price data (section 33 to 35)

---
+++ src/main/java/com/apachesparkforjavadevelopers/ml/HousePriceAnalysis.java
@@ -0,0 +1,85 @@
+package com.apachesparkforjavadevelopers.ml;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.ml.evaluation.RegressionEvaluator;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.param.ParamMap;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.tuning.ParamGridBuilder;
+import org.apache.spark.ml.tuning.TrainValidationSplit;
+import org.apache.spark.ml.tuning.TrainValidationSplitModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import static org.apache.spark.sql.functions.col;
+
+public class HousePriceAnalysis {
+
+    public static void main(String[] args) {
+
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkSession spark = SparkSession.builder().appName("HousePriceAnalysis").master("local[*]")
+                .config("spark.sql.warehouse.dir", "file:///C:/Users/mheinecke/spark_tmp")
+                .getOrCreate();
+
+        Dataset<Row> csvData = spark.read()
+                .option("header", true)
+                .option("inferSchema", true)
+                .csv("src/main/resources/kc_house_data.csv")
+                .drop("id", "date", "waterfront", "view", "condition",
+                        "grade", "yr_renovated", "zipcode", "lat", "long",
+                        "sqft_lot", "sqft_lot15", "yr_built", "sqft_living15",
+                        "sqft_basement")
+                .withColumn("sqft_above_share", col("sqft_above").divide(col("sqft_living")));
+
+        VectorAssembler vectorAssembler = new VectorAssembler()
+                .setInputCols(new String[]{"bedrooms", "bathrooms", "sqft_living", "sqft_above_share", "floors"})
+                .setOutputCol("features");
+        Dataset<Row> csvDataWithFeatures = vectorAssembler.transform(csvData);
+
+        Dataset<Row> modelInputData = csvDataWithFeatures
+                .select("price", "features")
+                .withColumnRenamed("price", "label");
+
+//        modelInputData.show(false);
+
+        Dataset<Row>[] dataSplits = modelInputData.randomSplit(new double[]{0.8, 0.2});
+        Dataset<Row> trainingAndTestData = dataSplits[0];
+        Dataset<Row> holdOutData = dataSplits[1];
+
+        LinearRegression linearRegression = new LinearRegression();
+
+        // helper object to build ParamMap object
+        ParamGridBuilder paramGridBuilder = new ParamGridBuilder();
+
+        // ParamMap is a grid of parameter combinations for Spark to test which one yields the best model
+        ParamMap[] paramMap = paramGridBuilder
+                .addGrid(linearRegression.regParam(), new double[]{0.01, 0.1, 0.5})
+                .addGrid(linearRegression.elasticNetParam(), new double[]{0, 0.5, 1})
+                .build();
+
+        TrainValidationSplit trainValidationSplit = new TrainValidationSplit()
+                .setEstimator(linearRegression) // param is type of model builder
+                .setEvaluator(new RegressionEvaluator().setMetricName("r2")) // set metric for model evaluation
+                .setEstimatorParamMaps(paramMap)
+                .setTrainRatio(0.8); // ratio for training and test data split
+
+        TrainValidationSplitModel model = trainValidationSplit.fit(trainingAndTestData);
+        LinearRegressionModel lrModel = (LinearRegressionModel) model.bestModel();
+
+        System.out.println("The training data r2 value is " + lrModel.summary().r2());
+        System.out.println("The training data RMSE value is " + lrModel.summary().rootMeanSquaredError());
+
+        System.out.println("The test data r2 value is " + lrModel.evaluate(holdOutData).r2());
+        System.out.println("The test data RMSE value is " + lrModel.evaluate(holdOutData).rootMeanSquaredError());
+
+        System.out.println("Intercept: " + lrModel.intercept() + "; coefficient: " + lrModel.coefficients());
+        System.out.println("regPram: " + lrModel.getRegParam() + "; elastic net param: " + lrModel.getElasticNetParam());
+
+    }
+
+}
